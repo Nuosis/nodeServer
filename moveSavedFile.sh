3d2cae7062d0883a79d7d18260d10764fcb12038
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-11-26 15:34:39 -0400

    yet again

--- moveSavedFile.sh
+++ moveSavedFile.sh
@@ -13,9 +13,9 @@ if [ "$#" -lt 1 ]; then
 fi
 
 PDF_PATH="$1"
-"  : PDF_PATH: $PDF_PATH" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
-if [ $PDF_PATH =="Undefined" ]; then
-"  : PDF_PATH: Cannot be Empty" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
+echo "  : PDF_PATH: $PDF_PATH" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
+if [ "$PDF_PATH" == "undefined" ]; then
+    echo "  : PDF_PATH: Cannot be Empty" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
     echo "$timestamp: END" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
     exit 1
 fi
@@ -27,13 +27,13 @@ if mv "$PDF_PATH" "$DESTINATION_PATH"; then
     echo "  : File moved successfully." >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
     
     # If debug is set, move the file back
-    if [ "$DEBUG" == "1" ]; then
+    if [ "${DEBUG:-0}" == "1" ]; then
         mv "$DESTINATION_PATH/$(basename "$PDF_PATH")" "$PDF_PATH"
         echo "  : File returned due to debug mode." >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
     fi
 else
     # Capture the last error message
-    errorMessage=$?
+    errorMessage=$(mv "$PDF_PATH" "$DESTINATION_PATH" 2>&1)
 
     echo "  : Failed to move file. Error code: $errorMessage" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
     echo "$timestamp: END" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
