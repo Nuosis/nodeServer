22c34a21a746a7cf3b54a8c707d222defee6438b
Marcus Swift <marcus@claritybusinesssolutions.ca>
2023-11-26 10:48:37 -0400

    further moveFIle modifications

--- moveSavedFile.sh
+++ moveSavedFile.sh
@@ -2,39 +2,36 @@
 
 # Script to move a PDF from a temporary location to the web server's image folder
 
+# Variables
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$timestamp: START" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
 # Ensure the file path is passed as an argument
 if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <path_to_pdf>" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut' 2>&1
+    echo "  Usage: $0 <path_to_pdf>" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut' 2>&1
     exit 1
 fi
 
-# Variables
 PDF_PATH="$1"
-echo "$timestamp: PDF_PATH: $PDF_PATH" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
 DEBUG="$2"
-echo "$timestamp: DEBUG: $DEBUG" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
 DESTINATION_PATH="/Library/FileMaker Server/HTTPServer/htdocs/httpsRoot/images"
-timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+echo "  : PDF_PATH: $PDF_PATH" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
+echo "  : DEBUG: $DEBUG" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
 
 # Move the file
-mv "$PDF_PATH" "$DESTINATION_PATH"
-
-# Check if the move was successful
-if [ $? -eq 0 ]; then
-    echo "$timestamp: File moved successfully." >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
-    if [ "DEBUG" == 1 ]; then
-        mv "$DESTINATION_PATH" "$PDF_PATH" 
+if mv "$PDF_PATH" "$DESTINATION_PATH"; then
+    echo "  : File moved successfully." >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
+    
+    # If debug is set, move the file back
+    if [ "$DEBUG" == "1" ]; then
+        mv "$DESTINATION_PATH/$(basename "$PDF_PATH")" "$PDF_PATH"
+        echo "  : File returned due to debug mode." >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
     fi
-    echo "$timestamp: File moved successfully." >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
 else
     # Capture the last error message
-    errorMessage=$(mv "$PDF_PATH" "$DESTINATION_PATH" 2>&1)
+    errorMessage=$?
 
-    echo "$timestamp: Failed to move file. Error: $errorMessage" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
+    echo "  : Failed to move file. Error code: $errorMessage" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
     exit 1
 fi
-
-
-# EXAMPLE CLI  /Library/FileMaker\ Server/Data/Scripts/moveSavedFile.sh <<PATH TO FILE>>
-# macOS FileMaker Use the "Perform AppleScript" step with a calculation like this: "do shell script \"/Library/FileMaker\ Server/Data/Scripts/moveSavedFile.sh " & YourFilePathVariable & "\""
-# winOS Use the "Send Event" script step with a calculation similar to this: "cmd /c \"/path/to/your/script.bat " & YourFilePathVariable & "\""
+echo "$timestamp: END" >> '/Library/FileMaker Server/Data/Documents/moveFileStdOut'
